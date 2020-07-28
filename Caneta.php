d8f75186fbad84f7404fbc260048b5b40b6eedb1
Karlanni <cristina.karlanni@gmail.com>
2020-07-28 18:48:03 -0300

    first commit Criando classes e objetos aula #02b

---
+++ Caneta.php
@@ -0,0 +1,37 @@
+<?php
+
+class Caneta {
+
+    var $modelo;
+    var $cor;
+    var $ponta;
+    var $carga;
+    var $tampa;
+
+    function rabiscar () {
+
+        if ($this->tampada == true) {
+
+            echo "<p>Erro! Não posso rabiscar...</p>";
+
+        } else {
+
+            echo "<p>Estou rabiscando...</p>";
+        }
+
+    }
+
+    function tampar() {
+
+        $this->tampada = true;
+
+    }
+
+    function destampar() {
+
+        $this->tampada = false;
+
+    }
+}
+
+
